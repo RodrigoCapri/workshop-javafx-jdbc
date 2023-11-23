79af02f9cb5c6bac17ee3b45491395a8751e6213
RodrigoCapri <rodrigocamr90@gmail.com>
2023-11-23 20:30:54 -0300

    MainView Design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,10 +17,14 @@ public class Main extends Application {
 			
 			//Carrega a main view fxml
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
+			ScrollPane scrollPane = loader.load();
+			
+			//Comandos para resolver a redimensionação do menu bar
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
 			//Cena principal
-			Scene mainScene = new Scene(parent);
+			Scene mainScene = new Scene(scrollPane);
 			
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
